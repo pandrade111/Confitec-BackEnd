c79dbc7df5395249cab5e5b6c1092e5a6569383a
Paulo Fernando A. de Brito Andrade <pandrade111@gmail.com>
2019-05-17 21:48:32 -0300

    .

--- Confitec/Confitec.Shared/Settings.cs
+++ Confitec/Confitec.Shared/Settings.cs
@@ -2,6 +2,6 @@
 {
     public static class Settings
     {
-        public static string ConnectionString = @"Server=baitresource.database.windows.net;Database=testeconfitec;User ID=baitresource;Password=B@iT830415;";
+        public static string ConnectionString = @"Server=baitresource.database.windows.net;Database=testeconfitec;User ID=***;Password=***;";
     }
 }
